479de0d000fa1092d67c34e2658e5668d4093846
Karnita-Saumya <karnitasaumya@gmail.com>
2026-05-09 00:55:23 +0530

    fix: remove duplicate healthz route to avoid endpoint conflict

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
 from server import app
 
 
-@app.route('/healthz')
-def healthz():
-    from flask import jsonify
-
-    return jsonify({'status': 'ok'})
-
-
 if __name__ == '__main__':
     import os
 
